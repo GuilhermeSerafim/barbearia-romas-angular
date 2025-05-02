b2bb855dc44d5fa631443c8e1dc82bcbbe9f45af
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-02 12:02:11 -0300

    Ajuste landing page - quem somos

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -5,11 +5,12 @@ import { ServicesComponent } from "../../components/services/services.component"
 import { GalleryComponent } from "../../components/gallery/gallery.component";
 import { LocalizationComponent } from "../../components/localization/localization.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { AboutComponent } from "../../components/about/about.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, ServicesComponent, GalleryComponent, LocalizationComponent, FooterComponent],
+  imports: [NavbarComponent, HeroComponent, ServicesComponent, GalleryComponent, LocalizationComponent, FooterComponent, AboutComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
