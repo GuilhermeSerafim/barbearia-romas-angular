5c16a9c25739578de4aff9972c5f7edcd582da89
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-05 11:53:00 -0300

    Criando funcionalidade de deletar cliente em adm > galeria

--- src/app/components/card-galeria-adm/card-galeria-adm.component.ts
+++ src/app/components/card-galeria-adm/card-galeria-adm.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input } from '@angular/core';
+  import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-@Component({
-  selector: 'app-card-galeria-adm',
-  standalone: true,
-  imports: [],
-  templateUrl: './card-galeria-adm.component.html',
-  styleUrl: './card-galeria-adm.component.css'
-})
-export class CardGaleriaAdmComponent {
-  @Input({ alias: 'path', required: true }) pathimg: string = "";
-  @Input({ alias: 'alt' }) altImg: string = "";
-  @Input({ alias: 'comment', required: true }) comment: string = "";
-  @Input({ alias: 'name', required: true }) name: string = "";
-  excluirItemGaleria() {
-    throw new Error('Method not implemented.');
+  @Component({
+    selector: 'app-card-galeria-adm',
+    standalone: true,
+    imports: [],
+    templateUrl: './card-galeria-adm.component.html',
+    styleUrl: './card-galeria-adm.component.css'
+  })
+  export class CardGaleriaAdmComponent {
+    @Input({ alias: 'path', required: true }) pathimg: string = "";
+    @Input({ alias: 'alt' }) altImg: string = "";
+    @Input({ alias: 'comment', required: true }) comment: string = "";
+    @Input({ alias: 'name', required: true }) name: string = "";
+    @Input({ alias: 'index', required: true }) index: number = 0;
+    @Input() teste: string = "";
+    @Output() deleteClientEmmit = new EventEmitter<number>();
+    emitirItemASerExcluido() {
+      console.log(this.index);
+      this.deleteClientEmmit.emit(this.index);
+    }
+    editarItemGaleria() {
+      throw new Error('Method not implemented.');
+    }
   }
-  editarItemGaleria() {
-    throw new Error('Method not implemented.');
-  }
-}
--- src/app/pages/admin-galeria/admin-galeria.component.ts
+++ src/app/pages/admin-galeria/admin-galeria.component.ts
@@ -11,5 +11,28 @@ import { CardGaleriaAdmComponent } from "../../components/card-galeria-adm/card-
   styleUrl: './admin-galeria.component.css'
 })
 export class AdminGaleriaComponent {
-
+  listClient = [
+    {
+      comment: "Sai daqui me sentindo outro cara, corte alinhado e barba na régua.",
+      name: "João S",
+      img: "/cliente1.png",
+      path: "/cliente1.png",
+      alt: "Cliente João"
+    },
+    {
+      comment: "O corte ficou simplesmente perfeito! O Romário manda muito na tesoura, super recomendo!",
+      name: "Cliente E",
+      img: "/cliente2.png",
+      alt: "Cliente Evelyn"
+    },
+    {
+      comment: "Levei meu filho pra cortar o cabelo e fiquei impressionado com o cuidado e paciência.",
+      name: "Letícia M",
+      img: "/cliente3.png",
+      alt: "Cliente Letícia"
+    },
+  ]
+  deleteCliente(i: number) {
+    this.listClient.splice(i, 1);
+  }
 }
