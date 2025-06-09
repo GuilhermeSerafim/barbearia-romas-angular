830d52af7420a9de870624b2a26b89bcade4f5dc
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-09 14:47:11 -0300

    Styling with Angular Material page admin login

--- src/app/pages/admin-login/admin-login.component.ts
+++ src/app/pages/admin-login/admin-login.component.ts
@@ -4,11 +4,15 @@ import { NavbarAdmComponent } from '../../components/adm/navbar-adm/navbar-adm.c
 import { FooterAdmComponent } from '../../components/adm/footer-adm/footer-adm.component';
 import { AdminService } from '../../services/admin.service';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [NavbarAdmComponent, FooterAdmComponent, FormsModule],
+  imports: [NavbarAdmComponent, FooterAdmComponent, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css'
 })
@@ -16,6 +20,7 @@ export class AdminLoginComponent {
   user = '';
   password = '';
   errorMsg = '';
+  formSubmitted = false;
 
   constructor(private router: Router, private adminService: AdminService) { }
 
@@ -24,23 +29,30 @@ export class AdminLoginComponent {
   }
 
   onSubmit() {
+    this.formSubmitted = true;
+
+    if (!this.user || !this.password) {
+      this.errorMsg = 'Preencha todos os campos.';
+      return;
+    }
+
     this.errorMsg = '';
-    this.adminService.login(this.user, this.password)
-      .subscribe({
-        next: ok => {
-          if (ok) {
-            // salva flag simples e redireciona
-            localStorage.setItem('isAdmin', '1');
-            this.router.navigate(['/adm/menu']);
-          } else {
-            this.errorMsg = 'Credenciais incorretas';
-          }
-        },
-        error: () => {
-          this.errorMsg = 'Erro ao conectar com o servidor';
+
+    this.adminService.login(this.user, this.password).subscribe({
+      next: ok => {
+        if (ok) {
+          localStorage.setItem('isAdmin', '1');
+          this.router.navigate(['/adm/menu']);
+        } else {
+          this.errorMsg = 'Credenciais incorretas';
         }
-      })
+      },
+      error: (e) => {
+        this.errorMsg = e.error.message ?? 'Erro ao conectar com o servidor';
+      }
+    });
   }
+
 
 }
 
