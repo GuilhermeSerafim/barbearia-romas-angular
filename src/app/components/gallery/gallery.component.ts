50ffa57dc9a4734bfc4def08e702b0e740496ee9
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-09 14:06:03 -0300

    Implementing component <Loading/> in component <Gallery/> of landing page

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CardGaleriaComponent } from "../card-galeria/card-galeria.component";
 import { GaleriaService } from '../../services/galeria.service';
 import { GaleriaItem } from '../../interfaces/galeria-item';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [CardGaleriaComponent],
+  imports: [CardGaleriaComponent, MatProgressSpinnerModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent implements OnInit {
+
+  carregando: boolean = true;
+
   constructor(private galeria: GaleriaService) { }
 
   itens: GaleriaItem[] = [];
@@ -20,9 +24,15 @@ export class GalleryComponent implements OnInit {
   }
 
   load() {
-    this.galeria.getAll().subscribe(data => {
-      console.log( this.itens = data)
-      return data
+    this.galeria.getAll().subscribe({
+      next: data => {
+        this.itens = data;
+        this.carregando = false;
+      },
+      error: err => {
+        console.error(err);
+        this.carregando = false;
+      }
     });
   }
 }
